9278daad48621ead89c2084587e6229097ee4785
synapse0426 <synapselee@gmail.com>
2019-09-23 17:15:59 +0900

    외부 api 연동 - 간단한 예시데이터 가지고 화면 띄우기.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './14_components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-      try{
-          const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=d6c614055e7f49a485d3c121fa70ec32',
-          );
-
-          setData(response.data);
-      } catch (e) {
-        console.log(e);
-      }
-  };
-  return (
-      <div>
-          <div>
-            <button onClick={onClick}>불러오기</button>
-          </div>
-          {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-      </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
