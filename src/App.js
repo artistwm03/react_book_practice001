05b37e866405b138bb858869db870bed3f56bf54
synapse0426 <synapselee@gmail.com>
2019-09-23 18:22:39 +0900

    1-3  category 상태 관리. useState 사용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import NewsList from './14_components/NewsList';
 import Categories from './14_components/Categories';
 
 const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category), []);
+
   return (
     <>
-      <Categories />
-      <NewsList />
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
     </>
 
   );
