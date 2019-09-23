600d0571258b240f72a196ace98e3599ea896563
synapse0426 <synapselee@gmail.com>
2019-09-23 17:58:59 +0900

    1-2 카테고리 목록 가져오기.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import NewsList from './14_components/NewsList';
+import Categories from './14_components/Categories';
 
 const App = () => {
-  return <NewsList />;
+  return (
+    <>
+      <Categories />
+      <NewsList />
+    </>
+
+  );
 };
 
 export default App;
