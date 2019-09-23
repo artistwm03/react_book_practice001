50b1ef0c841042dabf3e4f4ee0201ea561adaa2e
synapse0426 <synapselee@gmail.com>
2019-09-23 16:44:13 +0900

    외부 API를 연동하여 뉴스뷰어 만들기. 리액트를 다루는 기술 14장. api 사용해서 데이터 가져옴.

--- src/8_Hook/Info.js
+++ src/8_Hook/Info.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
   const [name, setName] = useState('');
-  const [nickName, setNickname] = useState('');
+  const [nickname, setNickname] = useState('');
   // useState 여러개 사용 가능.! 관리해야 되는 상태가 여러개라도 ok.
+  useEffect( () => {
+    console.log('렌더링 완료.');
+    console.log({
+      name,
+      nickname
+    });
+  }, []);
+  // useEffect 함수의 두번째 인자를 위 처럼 [] 비어있는 배열을 넣어주면,
+  // 설정해놓은 함수가 화면에 맨 처음 렌더링될 때만 실행 되고 , 업데이트될 때는 실행되지 않는다.
+  // , [] 를 지워보고 차이점 비교해보기. 개발자 도구 console 탭에서 차이점 확인 가능.
+
 
   const onChangeName = e => {
     setName(e.target.value);
@@ -22,18 +33,63 @@ const Info = () => {
     <div>
       <div>
         <input value={name} onChange={onChangeName} />
-        <input value={nickName} onChange={onChangeNickname}/>
+        <input value={nickname} onChange={onChangeNickname}/>
       </div>
       <div>
         <div>
           <b>이름:</b> {name}
         </div>
         <div>
-          <b>닉네임:</b> {nickName}
+          <b>닉네임:</b> {nickname}
         </div>
       </div>    
     </div>
   );
 };
+
+
+
+
+
+
+function increase(number) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const result = number +10;
+        if (result > 50) {
+          const e = new Error('NumberTooBig');
+          return reject(e);
+        }
+        resolve(result);
+    }, 1000);
+  });
+  return promise;
+}
+
+
+async function runTasks() {
+  try{
+    let result = await increment(0);
+    console.log(result);
+    let result = await increment(0);
+    console.log(result);
+    let result = await increment(0);
+    console.log(result);
+    let result = await increment(0);
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+
+
+
+
+
+
+
+
+
 
 export default Info;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import Info from './8_Hook/Info';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+      try{
+          const response = await axios.get(
+          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=d6c614055e7f49a485d3c121fa70ec32',
+          );
+
+          setData(response.data);
+      } catch (e) {
+        console.log(e);
+      }
+  };
   return (
-  <Info />
+      <div>
+          <div>
+            <button onClick={onClick}>불러오기</button>
+          </div>
+          {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+      </div>
   );
-}
-
+};
 
 export default App;
-
