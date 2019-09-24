e866bc5547631ba905a78bde6c1f2c0ab70185f9
synapse0426 <synapselee@gmail.com>
2019-09-24 12:42:33 +0900

    13장-1 1) 프로젝트에 라우터 적용 2) 페이지 만들기(Home,About)

---
+++ src/13_Router/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>홈</h1>
+      <p>홈, 그 페이지는 가장 먼저 보여지는 페이지.</p>
+    </div>
+  );
+};
+// 맨 처음 보여줄 놈
+
+export default Home;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import NewsPage from './14_2_pages/NewsPage';
+import About from './13_Router/About';
+import Home from './13_Router/Home';
 
 const App = () => {
-  return <Route path="/:category?" component={NewsPage} />;
+  return (
+    <div>
+      <Route path="/"      component={Home} />
+      <Route path="/about" component={About} />
+    </div>
+  );
 };
 
+// 프로젝트에 라우터 적용.
+// yarn add react-router-dom 안돼있을 경우 실행.
 export default App;
