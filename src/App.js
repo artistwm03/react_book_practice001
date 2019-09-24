39501f4793d6289e3d5b5ea3bd6a1474735b38a7
synapse0426 <synapselee@gmail.com>
2019-09-24 11:08:35 +0900

    라우터1-3 카테고리값을 url 을 통해서 받아오게 되니, 선택된 카테고리값 알려줄 필요도 없고, onsSelect 함수로 필요 없게 됨.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import React, { useState, useCallback } from 'react';
-import NewsList from './14_components/NewsList';
-import Categories from './14_components/Categories';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './14_2_pages/NewsPage';
 
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback(category => setCategory(category), []);
-
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
-
-  );
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
