190bf664ad2cb12e9f09c9c7e1fb0f5286918a54
synapse0426 <synapselee@gmail.com>
2019-09-24 14:17:22 +0900

    13장-3  LInk 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import About from './13_Router/About';
 import Home from './13_Router/Home';
 
 const App = () => {
   return (
     <div>
-      <Route path="/"      component={Home} exact={true}/>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
     </div>
   );
@@ -17,4 +26,6 @@ const App = () => {
 
 // exact 값을 true 로 준 이유는 ->  두 경로에  / 가 공통으로 들어가 있기 때문에 화면에 두 컴포넌트가 실행된다.
 // 따라서 이를 방지하기 위해서 exact 값을 true 로 준것이다. 그러면 /about 입력하면 About 컴포넌트에 대한 내용만 출력된다.
+
+// Link 컴포넌트 : 클릭하면 다른 주소로 이동시켜 주는 컴포넌트 , 페이지의 주소만 변경해 준다.(페이지를 새로 불러오지 않음.)
 export default App;
