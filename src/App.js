f6b18d59d13a07f3fdd2b60acd5952c3168e99bf
synapse0426 <synapselee@gmail.com>
2019-09-24 12:55:34 +0900

    13장-2 경로에 동일한 규칙이 있는 경우 어떻게 분리시키는지 알 수 있다.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './13_Router/Home';
 const App = () => {
   return (
     <div>
-      <Route path="/"      component={Home} />
+      <Route path="/"      component={Home} exact={true}/>
       <Route path="/about" component={About} />
     </div>
   );
@@ -14,4 +14,7 @@ const App = () => {
 
 // 프로젝트에 라우터 적용.
 // yarn add react-router-dom 안돼있을 경우 실행.
+
+// exact 값을 true 로 준 이유는 ->  두 경로에  / 가 공통으로 들어가 있기 때문에 화면에 두 컴포넌트가 실행된다.
+// 따라서 이를 방지하기 위해서 exact 값을 true 로 준것이다. 그러면 /about 입력하면 About 컴포넌트에 대한 내용만 출력된다.
 export default App;
